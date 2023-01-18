0e1e9c54abdd33117c1a503f5dd75cd496386682
João Miguel Galvão <117314159+jmiguel8396@users.noreply.github.com>
2023-01-18 16:03:07 -0300

    new

--- cursojs4/aula.js
+++ cursojs4/aula.js
@@ -4,11 +4,11 @@
 // Não divisível por 3 ou 5 => entrada
 // Não é um número => 'Não é um número'
 
-const resultado = fizzBuzz();
+const resultado = fizzBuzz(15);
 console.log(resultado);
 
 function fizzBuzz (entrada){
-    if (typeof entrada !== 'number')
+    if (typeof entrada != 'number')
         return 'Não é um número'
     if (entrada % 3 === 0 && entrada % 5 === 0)
         return 'FizzBuzz'
---
+++ cursojs6/aula.js
@@ -0,0 +1,11 @@
+exibirAsteriscos(10);
+
+function exibirAsteriscos(linhas) {
+    for(let linha = 1; linhas <= linhas; linha++){
+        let padrao = '';
+        for (let i = 0; i < linha; i++)
+            padrao += '*';
+    }
+        console.log(padrao);
+}
+
